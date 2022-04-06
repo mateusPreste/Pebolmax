fffaa7f4205a5e48c4aa2d6e0b9a6045706fed96
Mateus Preste <mateusx.mk@gmail.com>
2022-04-05 21:57:01 -0400

    add fedora script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo add-apt-repository ppa:nilarimogard/webupd8 && sudo apt update &&
-sudo apt install streamlink && sudo add-apt-repository ppa:mc3man/mpv-tests &&
-sudo apt remove mpv && sudo apt autoremove && pip install -r requirements.txt
+sudo apt -y install streamlink && sudo add-apt-repository ppa:mc3man/mpv-tests &&
+sudo apt -y install mpv && pip install -r requirements.txt
