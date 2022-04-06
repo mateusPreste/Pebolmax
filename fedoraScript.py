b87fd2a5e6360cd70770127dedcccf323fae9469
Mateus Preste <mateusx.mk@gmail.com>
2022-04-05 21:52:24 -0400

    add fedora script,

---
+++ fedoraScript.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo dnf install streamlink && sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && sudo dnf install mpv mpv-libs && pip install -r requirements.txt
